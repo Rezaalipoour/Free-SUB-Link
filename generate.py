bd1e32a70da9d04565bce0634e7a62ab1c42cdc0
Reza Alipour <rewzaalipoour@gmail.com>
2025-03-27 21:47:09 +0330

    update generate.py

--- generate.py
+++ generate.py
@@ -1,5 +1,4 @@
 import requests
-import base64
 
 urls = [
     "https://raw.githubusercontent.com/10ium/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs.txt",
@@ -13,19 +12,16 @@ urls = [
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_4.txt"
 ]
 
-all_configs = ""
+all_configs = []
 
 for url in urls:
     try:
         response = requests.get(url, timeout=10)
         if response.status_code == 200:
-            content = response.text.strip()
-            if content:
-                decoded = base64.b64decode(content).decode('utf-8', errors='ignore')
-                all_configs += decoded.strip() + "\n"
+            lines = response.text.strip().splitlines()
+            all_configs.extend(line for line in lines if line.startswith(("vmess://", "vless://", "trojan://", "ss://", "ssr://")))
     except Exception as e:
         print(f"خطا در دریافت {url}: {e}")
 
-# ذخیره در فایل
 with open("sub.txt", "w", encoding="utf-8") as f:
-    f.write(all_configs.strip())
+    f.write("\n".join(all_configs))
